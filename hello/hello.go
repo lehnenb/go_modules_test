97bdfbefdff4e0577648a80cfcdf6ffaf4e84b0d
Bruno Lehnen Simões <bruno.lehnen@gmail.com>
2020-06-12 11:46:20 +0200

    Add print hour feature

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,17 @@
 package hello
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // SayHelloAndOneMoreThing function: Says hello
 func SayHelloAndOneMoreThing(name string) {
-	fmt.Printf("Hello World, %s", name)
+	printHours()
+	fmt.Println(fmt.Sprintf("Hello World, %s", name))
+}
+
+func printHours() {
+	hours, minutes, _ := time.Now().Clock()
+	fmt.Println(fmt.Sprintf("%d:%02d ", hours, minutes))
 }
